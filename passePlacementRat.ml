5034b627a090bb9f31c40d28a16d298620532b5b
COGNARD Clement <ccognard@griffon.enseeiht.fr>
2023-11-30 10:14:26 +0100

    update placement

--- passePlacementRat.ml
+++ passePlacementRat.ml
@@ -27,18 +27,31 @@ let rec analyse_placement_instruction i depl reg =
     | _ -> failwith("todo : pas une infoVar")
     end
   |AstType.Conditionnelle (c,b1,b2) -> 
-    let nb1 = analyse_placement_bloc reg depl nb1 in
-    let nb2 = analyse_placement_bloc reg depl nb2 in
+    let nb1 = analyse_placement_bloc b1 depl reg in
+    let nb2 = analyse_placement_bloc b2 depl reg in
     (AstPlacement.Conditionnelle (c, nb1, nb2),0)
+  |AstType.TantQue (e,b) ->
+    let il = analyse_placement_bloc b depl reg in
+    AstPlacement.TantQue (e,il),0
+    (*
+  |AstType.Retour (e, info_ast) -> 
+    begin
+    match info_ast_to_info info_ast with
+    |InfoFun (_, tr, tp) ->
+      let tailleTr = getTaille tr in
+      let tailleTp = List.fold_right + tp in
+      AstPlacement.Retour (e, tailleTr, tailleTp),0
+    | _  -> failwith ("pas une info fun")
+    end*)
   | _ -> failwith("pas encore fait") 
 
-and analyse_placement_bloc reg depl li = 
+and analyse_placement_bloc li depl reg = 
   match li with
-  |[] -> []
-  |i::q -> 
-    let (ni, ndep) = analyse_placement_instruction reg depl i in
-      ni :: (analyse_placement_bloc reg ndep q)
-  
+  |t::q -> 
+    let (nt,tailleT) = analyse_placement_instruction t depl reg in
+    let (nq,tailleQ) = analyse_placement_bloc q (depl+tailleT) reg in
+    nt::nq,tailleT+tailleQ
+  |[] -> [],0
 
 let rec analyse_placement_parametres depl li =
   match li with
@@ -49,12 +62,12 @@ let rec analyse_placement_parametres depl li =
   | _ -> failwith("aa") 
 
 let analyse_placement_fonction (AstType.Fonction(info_ast, infol, instl)) =
-  let nb = analyse_placement_bloc "LB" 3 instl in
+  let nb = analyse_placement_bloc instl 3 "LB" in
   let nlp = analyse_placement_parametres 0 (List.rev infol) in
-  AstPlacement.Fonction (info_ast, nb, nlp)
+  AstPlacement.Fonction (info_ast, nlp, nb)
 
 
-let analyser_Programme (lf, b) =
-  let mlf = analyse_placement_fonction lf in 
-  let mb  = analyse_placement_bloc b in
+let analyser  (AstType.Programme (lf, b)) =
+  let mlf = List.map analyse_placement_fonction lf in 
+  let mb  = analyse_placement_bloc b 0 "SB" in
   AstPlacement.Programme (mlf,mb)
